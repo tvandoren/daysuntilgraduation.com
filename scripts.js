9a8ad39900113f5d8977524024f2be5dd33bba5c
Trevor VanDoren <trevor@trevorvandoren.com>
2021-02-17 00:38:06 -0600

    simplified javascript for default grad date

--- scripts.js
+++ scripts.js
@@ -1,58 +1,53 @@
-const { DateTime, Interval } = luxon;
+// set up Luxon
+const { DateTime, Duration } = luxon;
+// give setInterval global scope
+let interval;
 
-function getTimeToGraduation( // defaults to my graduation time
-  timeZone = "America/Chicago",
-  year = 2021,
-  month = 5,
-  day = 1,
-  hour = 12,
-  minute = 0
-) {
-  // set time for graduation start
-  let graduationTime = DateTime.fromObject({
-    zone: timeZone,
-    year: year,
-    month: month,
-    day: day,
-    hour: hour,
-    minute: minute,
-  });
-
-  // get current time in graduation timezone
-  let nowInGradTimezone = DateTime.fromObject({ zone: timeZone });
-
-  // return time difference as a luxon Interval object
-  updateCount(Interval.fromDateTimes(nowInGradTimezone, graduationTime));
-  return Interval.fromDateTimes(nowInGradTimezone, graduationTime);
-}
-
-function updateCount(intervalToGraduation) {
+function updateCount(gradTime) {
   const UnitsOfTime = ["days", "hours", "minutes", "seconds"]; // element IDs and luxon time units
 
-  if (intervalToGraduation.isValid) {
-    // graduation has not happened yet
-    // convert Interval to luxon Duration for parsing time units
-    const timeToGraduation = intervalToGraduation.toDuration(UnitsOfTime);
+  // get Duration from now to graduation time
+  let timeToGraduation = gradTime.diffNow().shiftTo(...UnitsOfTime);
 
+  if (timeToGraduation.seconds > 0) {
+    // graduation has not happened yet, so update
     UnitsOfTime.forEach((elemId) => {
-      let elemText = timeToGraduation[elemId]
+      document.getElementById(elemId).innerText = timeToGraduation[elemId]
         .toString()
         .split(".")[0] // only show whole seconds
         .padStart(2, "0"); // keep at least two digits
-
-      document.getElementById(elemId).innerText = elemText;
     });
   } else {
-    // graduation has happened
+    // graduation has happened, so set timer to 0 and clear interval
+    clearInterval(interval);
+    UnitsOfTime.forEach((elemId) => {
+      document.getElementById(elemId).innerText = "00";
+    });
     // TODO: update UI with something congratulatory
+    console.log("congratulations");
   }
 }
 
 function initialize() {
-  let interval = getTimeToGraduation(); // call with defaults
+  let defaultGraduationTime = DateTime.fromObject({
+    zone: "America/Chicago",
+    year: 2021,
+    month: 5,
+    day: 1,
+    hour: 12,
+    minute: 0,
+  });
 
-  setInterval(function () {
-    updateCount(interval);
+  let userDate = window.localStorage.getItem("userGrad");
+  let gradDate;
+  if (userDate !== null) {
+    gradDate = DateTime.fromISO(userDate);
+  } else {
+    gradDate = defaultGraduationTime;
+  }
+
+  interval = setInterval(() => {
+    updateCount(gradDate);
   }, 1000);
 }
 
