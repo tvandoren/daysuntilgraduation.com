b3dfe8baed953d9321bb36ec9a2d333269243be4
Trevor VanDoren <trevor@trevorvandoren.com>
2021-02-18 23:28:56 -0600

    added set date functionality

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 // set up Luxon
 const { DateTime, Duration } = luxon;
 // give setInterval global scope
-let interval;
+let timerHandle;
 
 function updateCount(gradTime) {
   const UnitsOfTime = ["days", "hours", "minutes", "seconds"]; // element IDs and luxon time units
@@ -19,7 +19,7 @@ function updateCount(gradTime) {
     });
   } else {
     // graduation has happened, so set timer to 0 and clear interval
-    clearInterval(interval);
+    clearInterval(timerHandle);
     UnitsOfTime.forEach((elemId) => {
       document.getElementById(elemId).innerText = "00";
     });
@@ -30,6 +30,7 @@ function updateCount(gradTime) {
 }
 
 function initialize() {
+  // set up counter
   let defaultGraduationTime = DateTime.fromObject({
     zone: "America/Chicago",
     year: 2021,
@@ -47,9 +48,43 @@ function initialize() {
     gradDate = defaultGraduationTime;
   }
 
-  interval = setInterval(() => {
+  // call new function with grad date
+  setTimer(gradDate);
+
+  // set up modal controls
+  let dateModal = document.getElementById("set-date-modal");
+  // open modal
+  document.getElementById("open-modal").onclick = () => {
+    dateModal.style.display = "flex";
+  };
+  // close modal if user clicks off
+  window.onclick = (event) => {
+    if (event.target === dateModal) {
+      dateModal.style.display = "none";
+    }
+  };
+}
+
+function setTimer(gradDate) {
+  // clear timer handle if it's set
+  if (timerHandle !== null) {
+    clearInterval(timerHandle);
+  }
+
+  // set timer with new date
+  timerHandle = setInterval(() => {
     updateCount(gradDate);
   }, 1000);
 }
 
-function storeGradDate() {}
+function setUserGrad() {
+  let gradDate = document.getElementById("grad-time").value;
+
+  if (gradDate) {
+    // save date to local storage
+    window.localStorage.setItem("userGrad", gradDate);
+
+    // update timer
+    setTimer(DateTime.fromISO(gradDate));
+  }
+}
